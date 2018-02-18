96a9b0b36b577fc0998db86957217371eb1eab7d
Sergio <sgutierr@gmu.edu>
2018-02-18 08:09:52 -0500

    use method add

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
 	public static void main(String[]args){
 		System.out.println("hello world");
-		System.out.println("gangsta as fuck");
+		System.out.println(sum(x,y));
 	}
 	public static int sum(int x, int y){
 		return x +y;
